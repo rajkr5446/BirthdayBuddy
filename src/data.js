8da7383f834c22e42d126989b29095cc215217a3
Raja <rajkr5446@gmail.com>
2023-10-07 08:01:45 +0530

    Added images locally and updated Readme file

--- src/data.js
+++ src/data.js
@@ -1,32 +1,38 @@
+import img1 from "./assets/person-1.jpeg";
+import img2 from "./assets/person-2.jpeg";
+import img3 from "./assets/person-3.jpeg";
+import img4 from "./assets/person-4.jpeg";
+import img5 from "./assets/person-5.jpeg";
+
 export const persons = [
   {
     id: 1,
     name: "Bertie Yates",
     age: 29,
-    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    image: img1,
   },
   {
     id: 2,
     name: "Hester Hogan",
     age: 32,
-    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    image: img2,
   },
   {
     id: 3,
     name: "Larry Little",
     age: 36,
-    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    image: img3,
   },
   {
     id: 4,
     name: "Sean Walsh",
     age: 34,
-    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    image: img4,
   },
   {
     id: 5,
     name: "Lola Gardner",
     age: 29,
-    image: "https://www.course-api.com/images/people/person-5.jpeg",
+    image: img5,
   },
 ];
